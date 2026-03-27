82ab4feaff93a7fbeb4354c3a6d56d80e706c472
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-03-26 20:42:48 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -58,8 +58,7 @@ class AdultSlideLimitationValidator(SlideLimitationValidator):
 class Slide:
 
     def __init__(self, name: str, 
-                 limitation_class: Type[SlideLimitationValidator]) 
-    -> None:
+                 limitation_class: Type[SlideLimitationValidator]) -> None:
         self.name = name
         self.limitation_class = limitation_class
 
